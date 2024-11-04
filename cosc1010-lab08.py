a3b930b78fb4e89dd2d34d601a673f6bd513cb09
esvisser <evisser@uwyo.edu>
2024-11-04 23:39:02 +0000

    completed lab

--- cosc1010-lab08.py
+++ cosc1010-lab08.py
@@ -1,8 +1,8 @@
-# Your Name Here
+# ezra visser
 # UWYO COSC 1010
-# Submission Date
-# Lab XX
-# Lab Section:
+# Submission Date11/4/2024
+# Lab 08
+# Lab Section:14
 # Sources, people worked with, help given to:
 # your
 # comments
@@ -14,10 +14,21 @@
 # Other wise return the converted int or float 
 # Floats should only have one decimal point in them 
 
+def int_convert (string):
+    num=0
+    input=string
+    if input[0]=="-":
+       input=input.removeprefix('-')
+       num=num-int(input)
+       return num
+    elif input.isdigit():
+        num=num+int(input)
+        return num
+    else:
+        return False
+      
 
 print("*" * 75)
-
-
 # Point-slope y = mx + b
 # This is used in mathematics to determine what the value y would be for any given x
 # Where b is the y-intercept, where the line crosses the y-axis (x = 0)
@@ -37,6 +48,27 @@ print("*" * 75)
 # Exit on the word exit
 # Remember all inputs are strings, but the function needs ints or floats
 # Call your function and print the resulting list
+def equation(m,b,lower,upper):
+    y=0
+    for num in range(lower,upper):
+        y= (m*num)+b
+        print(y)
+
+while True:
+    user_input=input("enter the m and b for a y=mx+b equation, as well as the upper and lower bound for it to be evaluated on seperated by commas(ex.3,5,7,12). enter exit to stop ")
+    if user_input =='exit':
+        break
+    nums=user_input.split(',')
+    m=int_convert(nums[0])
+    b=int_convert(nums[1])
+    upper=int_convert(nums[2])
+    lower=int_convert(nums[3])
+    
+    if upper==False or lower==False:
+       print("invalid input")
+    else:
+        equation(m,b,upper,lower)
+    
 
 print("*" * 75)
 
@@ -48,3 +80,30 @@ print("*" * 75)
 # Create a loop like above to prompt the user for input for the three values
 # Create a second function that just does the square root operation 
     # If the number you are trying to take the square root of is negative, return null
+def quadform_plus(a,b,c):
+    y=(-b+((b**2)+(4*a*c))**0.5)/(2*a)
+    print(y)
+def quadform_minus(a,b,c):
+    y=(-b-((b**2)+(4*a*c))**0.5)/(2*a)
+    print(y)
+while True:
+    user_input=input("enter values for the variables in the quadratic formula seperated by comas(ex. 2,5,3).enter exit to stop ")
+    if user_input=='exit':
+        break
+    nums=user_input.split(',')
+    a=int_convert(nums[0])
+    b=int_convert(nums[1])
+    c=int_convert(nums[2])
+    quadform_plus(a,b,c)
+    quadform_minus(a,b,c)
+def sqrt(x):
+    if x>0:
+        print(x**0.5)
+    else:
+        return False
+while True:
+    user_input=input("enter a number to square rooted. enter exit to stop ")
+    if user_input=="exit":
+        break
+    num=int_convert(user_input)
+    sqrt(num)
